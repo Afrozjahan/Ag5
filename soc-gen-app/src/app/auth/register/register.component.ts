aaec4896913ba49be9971ff3eba54c4669894491
unknown <afrozjahan.mujawar@gmail.com>
2018-07-05 17:06:05 +0530

    reactive form module

--- soc-gen-app/src/app/auth/register/register.component.ts
+++ soc-gen-app/src/app/auth/register/register.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['../login/login.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
-
+  username = new FormControl('', [Validators.email, Validators.required, Validators.minLength(6)]);
+  password = new FormControl('', [Validators.required, this.hasExclaimation]);
+  registerForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: this.username,
+      password: this.password
+    })
+  }
   ngOnInit() {
+  }
+  hasExclaimation(input: FormControl) {
+    const excl = input.value.indexOf("!") >= 0;
+    return excl ? null : { 'needExclaimation': true };
+  }
+  register() {
+    console.log("registered");
   }
 
 }
